d386cc513904ca2eb6cdba00079398f5e1dd5578
Sinan Altaii <sinan.altaii@44tva.se>
2019-11-17 12:15:21 +0100

    initial commit

---
+++ Kattis.Tests/ProblemA.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Kattis.Tests
+{
+	public class ProblemA
+	{
+		[Theory]
+		[InlineData("bob")]
+		[InlineData("booob")]
+		public void GivenName_WhenContainsRepetitiveLetters_ThenOutputReturns_bob(string name)
+		{
+			const string expected = "bob";
+			var actual = GetName(name);
+			Assert.Equal(expected, actual);
+		}
+
+		[Theory]
+		[InlineData("boooooobapalaxxxxios")]
+		public void GivenName_WhenContainsRepetitiveLetters_ThenoutputReturns_bobapalaxios(string name)
+		{
+			const string expected = "bobapalaxios";
+			var actual = GetName(name);
+			Assert.Equal(expected, actual);
+		}
+
+		private static string GetName(string name)
+		{
+			var set = name.ToCharArray();
+			var addedChars = new char[name.Length];
+			var cleansedName = new List<char>(name.Length);
+			var firstChar = set.First();
+			var sb = new StringBuilder();
+
+			addedChars[0] = firstChar;
+			cleansedName.Add(firstChar);
+			sb.Append(firstChar);
+			for (var i = 0; i < set.Length; i++)
+			{
+				if (cleansedName.Last() == set[i])
+				{
+					continue;
+				}
+				cleansedName.Add(name[i]);
+				sb.Append(set[i]);
+			}
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ Kattis/ProgramB2.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace Kattis
+//{
+//	class Program
+//	{
+//		public static HashSet<string> anagrams = new HashSet<string>();
+//		public static void Main()
+//		{
+//			string line;
+//			while ((line = Console.ReadLine()) != null)
+//			{
+//				char[] arr = line.ToCharArray();
+//				Permute(arr, 0, arr.Length - 1);
+//				Console.WriteLine(anagrams.Count);
+//			}
+//		}
+
+//		private static void Swap(ref char a, ref char b)
+//		{
+//			char tmp = a;
+//			a = b;
+//			b = tmp;
+//		}
+
+//		// SwapUsingBitWise could be used instead of Swap as in the original solution.
+//		private static void SwapUsingBitWise(ref char a, ref char b)
+//		{
+//			if (a == b)
+//				return;
+//			a ^= b;
+//			b ^= a;
+//			a ^= b;
+//		}
+
+//		private static void Permute(char[] elements, int recursionDepth, int maxDepth)
+//		{
+//			if (recursionDepth == maxDepth)
+//			{
+//				Console.WriteLine(elements);
+//				anagrams.Add(new string(elements));
+//				return;
+//			}
+
+//			for (int i = recursionDepth; i <= maxDepth; i++)
+//			{
+//				Swap(ref elements[recursionDepth], ref elements[i]);
+//				Permute(elements, recursionDepth + 1, maxDepth);
+//				// backtrack
+//				Swap(ref elements[recursionDepth], ref elements[i]);
+//			}
+//		}
+//	}
+//}
+
